9b7a0380c712ff82aa66c05780b96f3495836337
Joe Mooring <joe.mooring@veriphor.com>
2025-02-23 13:34:59 -0800

    Create test site

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/*
+This file is present to satisfy a requirement of the Tailwind CSS IntelliSense
+extension for Visual Studio Code.
+
+https://marketplace.visualstudio.com/items?itemName=bradlc.vscode-tailwindcss
+
+The rest of this file is intentionally empty.
+*/
